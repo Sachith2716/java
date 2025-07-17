a8abc6565316489be5fd8b8514e348e1fd256acb
Sachith2716 <sachithuttarwar23@gmail.com>
2025-07-17 16:52:34 +0530

    First Program

---
+++ first_fundamentals/Helloworld.java
@@ -0,0 +1,14 @@
+public class Helloworld {
+
+    // if you want to run a java program, class and main method 
+    // are must
+    // public - access modifier
+    // static - no object is required to run program
+    // void - retrun nothing
+    // main - method name, String[] args
+    public static void main(String[] args) {
+        // method body
+        System.out.println("Hello World");
+    }
+
+}
