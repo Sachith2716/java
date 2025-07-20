acf49adc5edde9e9d7a2f4277619143d360e1f09
Sachith2716 <sachithuttarwar23@gmail.com>
2025-07-20 18:39:53 +0530

    Application Form Assessment2

---
+++ variables/Assessment2.java
@@ -0,0 +1,101 @@
+package variables;
+
+public class Assessment2 {
+        public static void main(String[] args){
+            String APPLICATION = "          APPLICATION FOR COLLEGE ADMISSION TEST          ";
+            String University = "            Vidya Jyothi Institute of Technology            ";
+            String TOA = "[] 1st year      [] TRANSFEREE      [] RETURNEE";
+            String LastName = "_________________";
+            String FirstName = "___________________";
+            String MiddleName = "____________________";
+            String Address = "__________________________________";
+            String DOB = "________________";
+            String Citizenship = "[]INDIAN     []OTHERS              ";
+            String CivilStatus = "[]Single     []MARRIED";
+            String Sex = "[] Male     []Female";
+            long ContactNumber ;
+            String Religion = "________________";
+            String NameOfSpouse = "___________";
+            long SpouseContactNumber;
+
+            String Parent1 = "     FATHER     ";
+            String Parent2 = "     MOTHER     ";
+            String FNAME ="_____________";
+            //String MNAME = "_____________";
+            String Occ="_____________";
+            long PContactNumber ;
+            String GuardianName = "____________";
+            long GuardianContactNumber;
+            String GAddress = "____________________";
+
+            String SHS= "___________________________";
+            String TRANSFEREE="FOR TRANSFEREE / TECH-VOC COURCE GRADUATE / 2nd COURCE";
+            String SHSStrand= "_____________";
+            String LSA ="___________________";
+            int GraduateYear ;
+            double avg;
+            String Cource = "______________";
+            int LSY;
+
+
+            String ALS="[]Yes    []No";
+            int YearToke;
+            int Average;
+            String FourPeople = "[]Yes    []No";
+            long IDs;
+            String PWD="[]Yes    []No";
+            String PWDType="________________";
+
+
+
+
+            System.out.println(APPLICATION);
+            System.out.println(University);
+            System.out.println(TOA);
+            System.out.println("Last Name: "+LastName);
+            System.out.println("First Name: "+FirstName);
+            System.out.println("Middle Name: "+MiddleName);
+            System.out.println("Address: "+Address);
+            System.out.println("DOB: "+DOB);
+            System.out.print("Citizenship: "+Citizenship);
+            System.out.println("Civil Status: "+CivilStatus);
+            System.out.println("Sex: "+Sex);
+            System.out.print("Contact Number: "+"___________"+"      ");
+            System.out.println("Religion: "+Religion);
+            System.out.print("Name Of Spouse: "+NameOfSpouse+ "       ");
+            System.out.println("Spouse Contact Number: "+"________________");
+            System.out.println();
+
+
+            System.out.print("          "+Parent1+"          ");
+            System.out.println("          "+Parent2+"           ");
+            System.out.println("Name: "+ FNAME+"                               "+ FNAME );
+            System.out.println("Occupication: "+Occ+"                      "+Occ);
+            System.out.println("Contact Number: "+"__________"+"                          "+"__________");
+            System.out.print("Guardian Name: "+GuardianName+"    ");
+            System.out.println("Guardian Contact Number: "+"_____________");
+            System.out.println("Guardian Address: "+ GAddress);
+            System.out.println();
+
+            System.out.print("Senior High School: "+SHS+ "    ");
+            System.out.println(TRANSFEREE);
+            System.out.print("SHSStrand"+SHSStrand+"                   ");
+            System.out.println("Last School Attended: "+LSA);
+            System.out.print("Graduate Year: "+"_________"+"  ");
+            System.out.print("Gen.Avg: "+"_______"+"   ");
+            System.out.print("Cource: "+Cource+"  ");
+            System.out.println("Last School Year: "+"________");
+            System.out.println();
+
+            System.out.print("Are you an ALS Passer: "+ALS+"   ");
+            System.out.print("Year Token"+"_________"+"   ");
+            System.out.println("Avg: "+"_______");
+            System.out.print("Family a 4person Beneficier"+ FourPeople+"   " );
+            System.out.println("4Ps ID.no: "+"____");
+            System.out.print("Disablity or PWD"+PWD+"   ");
+            System.out.println("Type of Disablity"+PWDType);
+
+
+    }
+
+}
